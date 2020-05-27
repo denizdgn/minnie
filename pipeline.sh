3453c1271e0a0bf1ae7cb8230d143dfc7ade0c23
Deniz Dogan <deenizzdogan@gmail.com>
2020-05-27 17:05:01 +0300

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -36,4 +36,5 @@ do
 
   sed 's/,/ /g' "$projectID_ring_stacking_compared_25_perres.csv" | grep "protein-dna" | grep ":" | awk '{printf "show sticks, (resi %s and chain %s) + (resi %s and chain %s) \n", $6,$2,$7,$3}' | sort -u > pymol_ring_stacking.pml
   sed 's/,/ /g' "$projectID_ring_stacking_compared_25_perres.csv" | grep "protein-dna" | grep ":" | awk '{printf "distance i. %s and n. %s and chain %s, i. %s and n. %s and chain %s\n", $6,$8,$2,$7,$9,$3}' | sort -u | sed 's/OP1/O1P/g' | sed 's/OP2/O2P/g' >> pymol_ring_stacking.pml
+  cd -
 done
