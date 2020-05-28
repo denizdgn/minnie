1ce2adef9eed58457b4226514edfac854cf79650
Deniz Dogan <deenizzdogan@gmail.com>
2020-05-28 15:32:58 +0300

    updated parameters

--- minnie.py
+++ minnie.py
@@ -59,7 +59,7 @@ def splitpdbs(self):
 
               f'                        -p, --pdbs             [<traj.pdb>] [<traj.pdb>]   \n'
               f'                                               Trajectory ID of your complex(s)\033[0m \n\n\n\n'
-          
+
               f'\n\033[1mUsage example:\033[0m\n\n'
               " minnie splitpdbs -cn sox4 sox18 -p sox4.pdb sox18.pdb \n"
               " minnie splitpdbs -cn sox4  -p sox4.pdb  \n")
@@ -96,19 +96,19 @@ def findbonds(self):
             f'\n\033[1mUsage: minnie findbonds \n'
               f'                        -cn, --complexName     <string>     \n '
               f'                                               Project ID of your complex\n\n'
-              
+
               f'                        -p, --pdbs             [<.pdb>/<path>] (singleframe.pdb)   \n'
               f'                                               Give single *.pdb or give folder path \n\n'
-              
+
               f'                        -i                     [<hbonds>/<ionic>/<hydrophobic>/<ring_stacking>/<all>] (hbonds)    \n'
               f'                                               Calculates which types of interactions \n\n'
-              
+
               f'                        -d                      <float> (2.5)                 \n'
               f'                                               Cut-off to define a hydrogen bond\n\n'
-              
+
               f'                        -intra, --includeIntra [<"True">/<"False">] ("False")  \n'
               f'                                               What do you want to analyze, all or only inter-monomer contacts? \033[0m \n\n\n\n'
-                          
+
               f'\n\033[1mUsage example:\033[0m\n\n'
               " Single frame    - minnie findbonds -cn sox4 -p sox4/02_frames/md_0.pdb -i hbonds  -s False  \n"
               " Multiple frames - minnie findbonds -cn sox4 -p sox4/02_frames/* -i hbonds \n"
@@ -165,7 +165,7 @@ def timefilter(self):
         print(f'\n\033[1mUsage: minnie timefilter \n'
               f'                        -cn, --complexName     <string>      \n '
               f'                                               Project ID of your complex\n\n'
-              
+
               f'                         -f, --files            [<.csv>]               \n'
               f'                                                Files of your complex\n\n'
               f'                         --per                  <float>                \n'
@@ -206,7 +206,7 @@ def compareCX(self):
               f'                                               Project ID of your complex(s)\n\n'
               f'                        --per                  <float>                \n'
               f'                                                Observation frequency to classify an interaction as critical\033[0m \n\n\n\n'
-              
+
               f'\n\033[1mUsage example:\033[0m\n\n'
               " minnie compareCX -cn sox4 sox18 --per 25  \n")
     else:
@@ -225,76 +225,82 @@ def compareCX(self):
 @option('-s', choices=["specific","common"],default="specific", dest="spp")
 @option('-i', choices=["hbonds","ionic","hydrophobic","ring_stacking","all"],default="hbonds",
                      dest="intType", help="choose interaction type")
-@option('-b',"--between", choices=["protein-dna","all"], dest="between",default="all")
+@option('-b',"--between", choices=["protein-dna","all"], dest="between")
 @option('--filename', dest="filename",help="filename to use while writing graph",default="")
 @subcommand()
 def graph(self):
     """aaaaand graphs!"""
-    try:
-        if self.help:
-            print(f'\n\033[1mUsage: minnie graph  \n'
-                  f'                        -cn, --complexName     <string> <string>     \n'
-                  f'                                               Project IDs of your complex(s)\n\n'
-                  
-                  f'                        --per                  <float>                \n'
-                  f'                                               Observation frequency to classify an interaction as critical \n\n'
-    
-                  
-                  f'                        -b, --between          [<protein-dna>/<all>] (all)   \n'
-                  f'                                               Between protein-dna or keep all \n\n'   
-                  
-                  f'                        -c, --chainIDs         <string> <string>       \n'
-                  f'                                               Give ChainIDs to proceed\n\n'
-                  
-                  f'                        --filename             <string>                           \n'
-                  f'                                               Give a name to output file (optional)\n\n'
-                  
-                  f'                        --colors               [<hex colors>] ("#D9B4CC", "#6F81A6")     \n'
-                  f'                                               Color IDs of the complexes (optional)\n\n'
-                  
-                  
-                  f'                        -i                     [<hbonds>/<ionic>/<hydrophobic>/<ring_stacking>/<all>] (hbonds)    \n'
-                  f'                                               Calculates which types of interactions \n\n'
-    
-                  f'                        -s                     [<specific>/<common>] (specific)                           \n'
-                  f'                                               Complex-specific or common interactions\033[0m \n\n\n\n'
-    
-                f'Please do not use "--between" and "--chainIDs" options at the same time\n\n'
-    
-                f'\n\033[1mUsage example:\033[0m\n\n'
-                  " minnie graph -cn 'sox4' 'sox18' --per 25 -i hbonds -s specific  -c A+B C --colors '#D9B4CC' '#6F81A6' \n"
-                  " minnie graph -cn 'sox4' 'sox18' --per 25 -i ionic -c A+B C  \n"
-                  " minnie graph -cn 'sox4' 'sox18' --per 25 -i ionic -b protein-dna \n"
-                  " minnie graph -cn 'sox4' 'sox18' --per 25 -i ionic -b protein-dna --filename sox4_sox18_protein_dna \n")
-        elif (self.between) and (self.chains):
-            raise Exception()
-        elif self.intType == "all":
-            for intTypex in ["hbonds","ionic","hydrophobic","ring_stacking"]:
-                if  (self.between) == "all":
-                    print(intTypex)
-                    df_collec=graphs.filter_todraw(self.complexName,self.chains,self.spp,self.per,str(intTypex))
-                    graphs.draw_fig(df_collec, str(intTypex), self.complexName[0], self.complexName[1],
-                                    self.colors[0], self.colors[1],self.filename, self.spp)
+    #try:
+    if self.help:
+        print(f'\n\033[1mUsage: minnie graph  \n'
+              f'                        -cn, --complexName     <string> <string>     \n'
+              f'                                               Project IDs of your complex(s)\n\n'
 
-                else:
-                    df_collec = graphs.filter_todnaall(self.complexName, self.between, self.spp, self.per, intTypex)
-                    graphs.draw_fig(df_collec, intTypex, self.complexName[0], self.complexName[1],
-                                    self.colors[0], self.colors[1], self.filename, self.spp)
+              f'                        --per                  <float>                \n'
+              f'                                               Observation frequency to classify an interaction as critical \n\n'
 
-        elif self.between == "protein-dna":
-            df_collec=graphs.filter_todnaall(self.complexName,self.between,self.spp,self.per,self.intType)
-            graphs.draw_fig(df_collec, self.intType, self.complexName[0], self.complexName[1],
-                            self.colors[0], self.colors[1],self.filename, self.spp)
 
-        else:
-            df_collec=graphs.filter_todraw(self.complexName,self.chains,self.spp,self.per,self.intType)
-            graphs.draw_fig(df_collec, self.intType, self.complexName[0], self.complexName[1],
-                            self.colors[0], self.colors[1],self.filename, self.spp)
-    except TypeError:
-        print(f'\nPlease check given parameters''')
+              f'                        -b, --between          [<protein-dna>/<all>] (all)   \n'
+              f'                                               Between protein-dna or keep all \n\n'
 
-    except Exception:
-        print(f'\nPlease, either use -b or -c option''')
+              f'                        -c, --chainIDs         <string> <string>       \n'
+              f'                                               Give ChainIDs to proceed\n\n'
+
+              f'                        --filename             <string>                           \n'
+              f'                                               Give a name to output file (optional)\n\n'
+
+              f'                        --colors               [<hex colors>] ("#D9B4CC", "#6F81A6")     \n'
+              f'                                               Color IDs of the complexes (optional)\n\n'
+
+
+              f'                        -i                     [<hbonds>/<ionic>/<hydrophobic>/<ring_stacking>/<all>] (hbonds)    \n'
+              f'                                               Calculates which types of interactions \n\n'
+
+              f'                        -s                     [<specific>/<common>] (specific)                           \n'
+              f'                                               Complex-specific or common interactions\033[0m \n\n\n\n'
+
+            f'Please do not use "--between" and "--chainIDs" options at the same time\n\n'
+
+            f'\n\033[1mUsage example:\033[0m\n\n'
+              " minnie graph -cn 'sox4' 'sox18' --per 25 -i hbonds -s specific  -c A+B C --colors '#D9B4CC' '#6F81A6' \n"
+              " minnie graph -cn 'sox4' 'sox18' --per 25 -i ionic -c A+B C  \n"
+              " minnie graph -cn 'sox4' 'sox18' --per 25 -i ionic -b protein-dna \n"
+              " minnie graph -cn 'sox4' 'sox18' --per 25 -i ionic -b protein-dna --filename sox4_sox18_protein_dna \n")
+    elif (self.between) and (self.chains):
+        raise Exception()
+    elif self.intType == "all":
+        print(self.between)
+        for intTypex in ["hbonds","ionic","hydrophobic","ring_stacking"]:
+
+            if  (self.between):
+                print(intTypex)
+                df_collec=graphs.filter_todnaall(self.complexName,self.between,self.spp,self.per,str(intTypex))
+                graphs.draw_fig(df_collec, str(intTypex), self.complexName[0], self.complexName[1],
+                                self.colors[0], self.colors[1],self.filename, self.spp)
+
+            elif (self.chains):
+                df_collec=graphs.filter_todraw(self.complexName,self.chains,self.spp,self.per,str(intTypex))
+                graphs.draw_fig(df_collec, str(intTypex), self.complexName[0], self.complexName[1],
+                                        self.colors[0], self.colors[1],self.filename, self.spp)
+
+    elif self.between == "protein-dna":
+        df_collec=graphs.filter_todnaall(self.complexName,self.between,self.spp,self.per,self.intType)
+        graphs.draw_fig(df_collec, self.intType, self.complexName[0], self.complexName[1],
+                        self.colors[0], self.colors[1],self.filename, self.spp)
+
+    elif self.between == "all":
+        df_collec=graphs.filter_todnaall(self.complexName,self.between,self.spp,self.per,self.intType)
+        graphs.draw_fig(df_collec, self.intType, self.complexName[0], self.complexName[1],
+                        self.colors[0], self.colors[1],self.filename, self.spp)
+    else:
+        df_collec=graphs.filter_todraw(self.complexName,self.chains,self.spp,self.per,self.intType)
+        graphs.draw_fig(df_collec, self.intType, self.complexName[0], self.complexName[1],
+                        self.colors[0], self.colors[1],self.filename, self.spp)
+    #except TypeError:
+    #    print(f'\nPlease check given parameters''')
+
+    #except Exception:
+    #    print(f'\nPlease, either use -b or -c option''')
 
 
 
@@ -310,6 +316,3 @@ if __name__ == "__main__":
         parser.print_help()
     else:
         args.func(args)
-
-
-
