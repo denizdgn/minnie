e49397f69b5f24a0bfc05ee6a910d06f70a43eed
Deniz Dogan <deenizzdogan@gmail.com>
2020-06-23 12:44:46 +0300

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 #Splitting trajectories into single frames
-minnie splitpdbs -cn sox4 sox18 -p sox4.pdb sox18.pdb
+minnie splitpdbs -i sox4 sox18 --pdbs sox4.pdb sox18.pdb
 
 # This part calculates all types of inter-monomer interactions
-minnie findbonds -cn sox4 -p sox4/01_frames/* -i all
-minnie findbonds -cn sox18 -p sox18/01_frames/* -i all
+minnie findbonds -i sox4 -d 'sox4/01_frames/' --itypes all --clean
+minnie findbonds -i sox18 -d 'sox18/01_frames/' --itypes all --clean
 
 # Apply critical interaction filter
-minnie timefilter -f sox4/02_interfacea_results/*/sox4_merged_*.csv -cn sox4 --per 25
-minnie timefilter -f sox18/02_interfacea_results/*/sox18_merged_*.csv -cn sox18 --per 25
+minnie timefilter -f sox4/02_interfacea_results/*/sox4_merged_*.csv -i sox4 --per 25
+minnie timefilter -f sox18/02_interfacea_results/*/sox18_merged_*.csv -i sox18 --per 25
 
 # Calculate common and distinct interactions in two cases
-minnie compareCX -cn sox4 sox18 --per 25
+minnie comparecx -i sox4 sox18 --per 25
 
 # aaaaand graphs!
-minnie graph -cn 'sox4' 'sox18' --per 25 -i all -b protein-dna -s specific
-minnie graph -cn 'sox4' 'sox18' --per 25 -i all -b protein-dna -s common
+minnie graph -i 'sox4' 'sox18' --per 25 --itypes all -b protein-dna -s specific
+minnie graph -i 'sox4' 'sox18' --per 25 --itypes all -b protein-dna -s common
 
 
 #To visualize minnie results with Pymol!
